48e3b98834b0d563bf6ecc851f8521252d396b68
Casey Conlin <101153261+CaseyConlin@users.noreply.github.com>
2023-05-23 06:57:38 -0400

    Update for push to git.

---
+++ postcss.config.js
@@ -0,0 +1,29 @@
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+
+module.exports = {
+  plugins:
+    process.env.NODE_ENV === "production"
+      ? [
+          "postcss-flexbugs-fixes",
+          [
+            "postcss-preset-env",
+            {
+              autoprefixer: {
+                flexbox: "no-2009",
+              },
+              stage: 3,
+              features: {
+                "custom-properties": false,
+              },
+            },
+          ],
+        ]
+      : [
+          // No transformations in development
+        ],
+};
