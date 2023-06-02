26558f08e1df4c2062e0e877750fcec40037ef55
Casey Conlin <101153261+CaseyConlin@users.noreply.github.com>
2023-06-02 10:23:44 -0400

    Configure tailwind to remove console error on posprocessing. Update postcss, globalcss.config and tailinwind.config.

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,8 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
         <p>[Your Self Introduction]</p>
 
         <p>
