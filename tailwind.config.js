ce1e5353845d460009321f031a4aaeac89d6632d
Casey Conlin <101153261+CaseyConlin@users.noreply.github.com>
2023-06-09 16:50:03 -0400

    Footer structure and design setup.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pd-blue": "rgb(0, 145, 203, 1)",
+        "pd-green": "rgb(161, 255, 161, 1)",
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms"), require("tailwindcss/nesting")],
 };
