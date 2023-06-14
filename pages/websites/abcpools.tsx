8598cfc03ec4298ec77c4f985c1521dad155fe56
Casey Conlin <101153261+CaseyConlin@users.noreply.github.com>
2023-06-14 08:07:50 -0400

    Update screenshots for website pages.

--- pages/websites/abcpools.tsx
+++ pages/websites/abcpools.tsx
@@ -13,7 +13,7 @@ export default function Abcpools() {
         title="ABC Pools & Spas"
         subtitle="Making a Splash with SEO and a Mobile-responsive Site"
         content="Their website helps customers find and hire ABC Pools & Spas for pool installations, service, and maintenance. Search Engine Optimization including fast load times for mobile devices puts their web page higher in search results. And contact forms and information connect customers easily to the business. Photos of ABC Pools' work show customers they can trust them to get the job done right."
-        videoUrl="https://embed-ssl.wistia.com/deliveries/7f7ab82fbbffb91bc1bd4cb45ebbf68c427b11e8/file.mp4"
+        videoUrl="https://embed-ssl.wistia.com/deliveries/a4255e54d029cd269f8ea9bcf7ee6bd8fadd4f05/file.mp4"
         siteUrl="https://abcpoolandspaservice.com/"
       />
       <ContactForm />
--- pages/websites/sissys.tsx
+++ pages/websites/sissys.tsx
@@ -14,7 +14,7 @@ export default function Sissyscafe() {
         title="Sissys Cafe"
         subtitle="To the Point with a POS Integration"
         content="Questions about restaurants usually center around hours, menu, and ordering, and the website for Sissy’s Cafe allows the information people are looking for to come to the surface. The site’s main call-to-action focuses on connecting folks to the point-of-sale integration to order food, which results in the cafe getting orders (and revenue) and visitors getting food (and less hungry) for a win-win situation. Folks looking for more background and up-to-date info on Sissy’s, an institution with its own story in Kingston’s Stockade District, will find this in the business’s about page, and the live Instagram feed."
-        videoUrl="https://embed-ssl.wistia.com/deliveries/7f7ab82fbbffb91bc1bd4cb45ebbf68c427b11e8/file.mp4"
+        videoUrl="https://embed-ssl.wistia.com/deliveries/5c26645ff37d03e193e44b3ac9204595fc27ef8b/file.mp4"
         siteUrl="https://sissyscafekingston.com/"
       />
       <ContactForm />
