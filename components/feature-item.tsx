00a073a3067fad13a87525633b86e8ccea8d2977
Casey Conlin <101153261+CaseyConlin@users.noreply.github.com>
2023-06-14 09:45:04 -0400

    Update feature items.

--- components/feature-item.tsx
+++ components/feature-item.tsx
@@ -27,12 +27,12 @@ export const FeatureItem = (props: {
         props.bgColor ? props.bgColor : "bg-white"
       } dark:bg-gray-900`}
     >
-      <div className="grid max-w-screen-xl px-4 py-8 mx-auto gap-10 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div className="mr-auto place-self-center lg:col-span-7">
+      <div className="grid my-2 max-w-screen-xl px-4 py-8 mx-auto gap-10 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7 p-2">
           <h3 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
             {props.title}
           </h3>
-          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+          <p className="max-w-2xl mb-6 font-light text-gray-800 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
             {props.content}
           </p>
 
@@ -57,7 +57,7 @@ export const FeatureItem = (props: {
           <a
             href={props.siteUrl}
             target="_blank"
-            className="inline-flex items-center justify-center px-5 py-3 text-base font-bold text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 hover:no-underline hover:scale-105 duration-500 transition-all"
+            className="inline-flex items-center justify-center px-5 py-3 text-base font-bold text-center text-gray-900 border border-gray-300 rounded-lg bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 hover:no-underline hover:scale-105 duration-500 transition-all"
           >
             Live Site
             <svg
